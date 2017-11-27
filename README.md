08db64df50e4b416d943840968130dad8e8b35cd
ikovic <kovic.ivan@gmail.com>
2017-11-27 22:27:32 +0100

    Plan

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gotitles
+
+A slow and lazy way to learn Go. Some planned features to demonstrate basic concepts:
+- search subtitles by hash
+- search subtitles by name
+- search from multiple resources
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("gotitles")
+	args := os.Args[1:]
+
+	client := http.Client{Timeout: time.Second * 10}
+
+	fmt.Println(strings.Join(args, " "))
 }
