727beb4f63976bfc305bab6a8bf856351ab7d2c6
ikovic <kovic.ivan@gmail.com>
2017-12-01 11:53:21 +0100

    Removed unused queue implementation. Calculate hash for video file

--- main.go
+++ main.go
@@ -7,18 +7,30 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/ikovic/gotitles/hash"
 	"gopkg.in/h2non/filetype.v1"
 )
 
-func walk(path string, info os.FileInfo, err error) error {
+func getHash(path string) uint64 {
+	file, _ := os.Open(path)
+	hash, _ := hash.HashFile(file)
+	return hash
+}
+
+func analyzeFile(path string, info os.FileInfo) {
 	if !info.IsDir() {
 		buf, _ := ioutil.ReadFile(path)
 		head := buf[:261]
 
 		if filetype.IsVideo(head) {
-			fmt.Println(path)
+			hash := getHash(path)
+			fmt.Println(hash)
 		}
 	}
+}
+
+func walk(path string, info os.FileInfo, err error) error {
+	go analyzeFile(path, info)
 	return nil
 }
 
@@ -27,4 +39,8 @@ func main() {
 	path := strings.Join(args, " ")
 
 	filepath.Walk(path, walk)
+
+	// don't stop
+	var input string
+	fmt.Scanln(&input)
 }
