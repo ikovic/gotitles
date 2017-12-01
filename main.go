4465d66fbe809a8bab70fa0c1649037194e63dd0
ikovic <kovic.ivan@gmail.com>
2017-12-01 11:23:19 +0100

    Use the built in function to walk the file tree. Use external package to determine video type

--- main.go
+++ main.go
@@ -2,51 +2,29 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/ikovic/gotitles/queue"
+	"gopkg.in/h2non/filetype.v1"
 )
+
+func walk(path string, info os.FileInfo, err error) error {
+	if !info.IsDir() {
+		buf, _ := ioutil.ReadFile(path)
+		head := buf[:261]
+
+		if filetype.IsVideo(head) {
+			fmt.Println(path)
+		}
+	}
+	return nil
+}
 
 func main() {
 	args := os.Args[1:]
-
 	path := strings.Join(args, " ")
-	stuff := []string{"test", "test3"}
-	// hash, err := hash.HashFile(file)
 
-	traverse(path, stuff)
-
-	// fmt.Println(hash)
-}
-
-func traverse(path string, nodes []string) {
-	file, err := os.Open(path) // For read access.
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	queue := new(queue.FileQueue)
-	queue.Enqueue(*file)
-
-	for !queue.IsEmpty() {
-		f := queue.Dequeue()
-		fileInfo, _ := f.Stat()
-		fmt.Println(fileInfo.Name())
-		// if dir, get children info
-		if fileInfo.IsDir() {
-			childrenNames, _ := f.Readdirnames(0)
-			for _, name := range childrenNames {
-				// find the exact path
-				filePath, _ := filepath.Abs(filepath.Dir(name))
-				fmt.Println(filePath)
-				child, _ := os.Open(filePath)
-				queue.Enqueue(*child)
-			}
-		}
-
-	}
-
+	filepath.Walk(path, walk)
 }
